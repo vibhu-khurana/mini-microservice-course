66f7f740bd8599e563ea981a80931b64ccd6cb62
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-06-14 19:25:47 +0530

    comments service created

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,28 +1,29 @@
+// @ts-nocheck
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-    console.log(req.body);
-    posts[id] = {
-        id,
-        title
-    };
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  console.log(req.body);
+  posts[id] = {
+    id,
+    title,
+  };
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-    console.log('Listening on 4000');
+  console.log('Listening on 4000');
 });
