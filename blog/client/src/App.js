d55387ad5a36c72523e9de1edc2be6f6df93f335
Vibhu Khurana <v-vibhu.khurana@blackbaud.com>
2020-06-21 22:16:00 +0530

    PostList component added

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,12 +1,16 @@
 // @ts-nocheck
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
   return (
     <div className='container'>
       <h1>Create Post</h1>
       <PostCreate />
+      <hr/>
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
